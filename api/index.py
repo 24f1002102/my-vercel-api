c0ad69c0e8e34405d50199bdc23eee43f9a1b21c
24f1002102 <24f1002102@ds.study.iitm.ac.in>
2025-10-05 00:18:51 +0530

    Add FastAPI latency endpoint

--- api/index.py
+++ api/index.py
@@ -9,8 +9,8 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],   # allow all HTTP methods
+    allow_methods=["POST", "OPTIONS"],  # allow POST and OPTIONS
+   # allow all HTTP methods
     allow_headers=["*"],
 )
 
